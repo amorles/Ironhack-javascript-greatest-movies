7b359ef812b54981c33ac0a8af17da9e68834e93
amorles <54360375+amorles@users.noreply.github.com>
2019-11-21 12:51:34 -0500

    It. 1 and 2 Done, yeehaw

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,9 +1,24 @@
-/* eslint no-restricted-globals: 'off' */
-
 // Iteration 1: Ordering by year - Order by year, ascending (in growing order)
 
-// Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct
+function orderByYear(inputArray){
+    let copy = [...inputArray];
+    copy.sort( (a, b) =>{
+    if (a.year > b.year) return 1;
+    else if (a.year < b.year) return -1;
+    else if (a.title > b.title) return 1;
+    else if (a.title < b.title) return -1;
+  }
+  )
 
+return copy;
+
+  }
+
+// Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct
+function howManyMovies(manyMovies) {
+    let ManyMoviesList = manyMovies.filter(movies => movies.director === "Steven Spielberg");
+    return ManyMoviesList.filter(movies => movies.genre.includes("Drama")).length;
+}
 // Iteration 3: Alphabetic Order - Order by title and print the first 20 titles
 
 // Iteration 4: All rates average - Get the average of all rates with 2 decimals
